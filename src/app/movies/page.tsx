f82152a44602280de6323e46875c9acf3c69e4f0
ehsan <ehsan.ghasemi@riwers.io>
2024-12-08 10:43:57 +0330

    clean up

---
+++ src/app/movies/page.tsx
@@ -0,0 +1,12 @@
+import {FC} from "react";
+
+type OwnProps = {
+
+}
+const MovieDetails: FC<OwnProps> = () => {
+  return(
+    <div>Movie details works !</div>
+  )
+}
+
+export default MovieDetails;
